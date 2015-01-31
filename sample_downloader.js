20ff130cf4addd53c12e28278bce667e2d43dc64
Sisil Mehta <sisilsmehta@gmail.com>
2015-01-30 21:25:56 -0800

    Added comments to sample_downloader.js

--- sample_downloader.js
+++ sample_downloader.js
@@ -2,16 +2,17 @@ var AWS = require('aws-sdk');
 var fs = require('fs');
 
 var s3 = new AWS.S3({
-    accessKeyId:"",
-    secretAccessKey:""
+    accessKeyId:"",    //required
+    secretAccessKey:"" //required
 });
 
 var params = {
-    Bucket:"",
-    Key:""
+    Bucket:"",        //required
+    Key:""            //required
 }
 var downloader = require('./index.js')(s3);
-var d = downloader.download(params,{totalObjectSize:68470352});
+var size = 0; //required: size of the object being downloaded
+var d = downloader.download(params,{totalObjectSize:size});
 d.on('error',function(err){
     console.log(err);
 });
